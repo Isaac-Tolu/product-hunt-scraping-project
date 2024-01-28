fb8f940cb3b72b73fdca7c958aafa12dd30622c0
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-01-28 16:07:40 +0100

    feat: changed approach to working with the links for filters and topics

--- main.py
+++ main.py
@@ -29,34 +29,62 @@ def get_top_products(tag):
 
         title_tag  = product_tag.find(class_=PRODUCT_TITLE__CLASS)
         extras_tag = product_tag.find(class_=PRODUCT_EXTRAS__CLASS)
-
-        d["link"] = title_tag.a["href"]
         title_content = list(title_tag.stripped_strings)
+        filters_and_topics = [a["href"] for a in extras_tag.find_all('a')]
+        print(filters_and_topics)
+        filters = [i.split("=")[-1] for i in filters_and_topics if i.startswith("/?filters")]
+        print(filters)
+        topics = [i.split("/")[-1] for i in filters_and_topics if i.startswith("/topics")]
+        print(topics)
+        print("\n\n")
+
         d["name"] = title_content[0]
         d["description"] = title_content[-1]
-
-        extras_content = list(extras_tag.stripped_strings)
-        if "Solo maker" in extras_content:
-            d["is_solo_maker"] = True
-            extras_content.remove("Solo maker")
-        else:
-            d["is_solo_maker"] = False
-        
-        if "Bootstrapped" in extras_content:
-            d["is_bootstrapped"] = True
-            extras_content.remove("Bootstrapped")
-        else:
-            d["is_bootstrapped"] = False
-        
-        topics = [i for i in extras_content if i != "•"]
-
+        d["link"] = title_tag.a["href"]
+        d["is_solo_maker"] = is_solo_maker(filters)
+        d["is_bootstrapped"] = is_bootstrapped(filters)
         d["topics"] = topics
+
+        
+        # if "Solo maker" in extras_content:
+        #     d["is_solo_maker"] = True
+        #     extras_content.remove("Solo maker")
+        # else:
+        #     d["is_solo_maker"] = False
+
+        # if "Bootstrapped" in extras_content:
+        #     d["is_bootstrapped"] = True
+        #     extras_content.remove("Bootstrapped")
+        # else:
+        #     d["is_bootstrapped"] = False
+        
+        # topics = [i for i in extras_content if i != "•"]
+
+        # d["topics"] = topics
 
         a.append(d)
 
     return a
     
-        
+## List -> Boolean
+## return true if the soloMaker filter is one of the URLs, otherwise false
+def is_solo_maker(l):
+    if "soloMaker" in l:
+        return True
+    return False
+
+## List -> Boolean
+## return true if the bootstrapped filter is one of the URLs, otherwise false
+def is_bootstrapped(l):
+    if "bootstrapped" in l:
+        return True
+    return False
+
+## List -> List
+## Extract the topics from their URLs and return them
+def get_topics(l):
+    ...
+
 
 if __name__ == "__main__":
     soup = get_ph_soup()
