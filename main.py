0f16750dcece45453e19e6f514b894902d343fde
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-01-30 14:51:20 +0100

    - refactor: changed the code structure to call functions for getting the titles and extras section - feat: extract the product owners - chore: clean up the code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ PARSER = 'html.parser'
 
 PRODUCT_SECTION__CLASS          = "styles_item__Dk_nz"
 PRODUCT_TITLE__CLASS            = "styles_titleContainer__qZRNa"
+PRODUCT_BY__CLASS               = "styles_container__EoS8Q"
 PRODUCT_EXTRAS__CLASS           = "styles_extraInfo__Xs_5Y"
 PRODUCT_VOTE_BUTTON__DATA_CLASS = "vote-button"
 PRODUCT_VOTE_BUTTON_CLASS       = "styles_voteCountItem__zwuqk"
@@ -37,21 +38,49 @@ def get_top_products(tag:Tag) -> list[dict]:
     for product_tag in tag.find_all(class_=PRODUCT_SECTION__CLASS):
         product_info = {}
 
-        title_tag  = product_tag.find(class_=PRODUCT_TITLE__CLASS)
+        title_tag = product_tag.find(class_=PRODUCT_TITLE__CLASS)
+        by_tag = product_tag.find(class_=PRODUCT_BY__CLASS)
         extras_tag = product_tag.find(class_=PRODUCT_EXTRAS__CLASS)
-        title_content = list(title_tag.stripped_strings)
-        extras_links = [a_tag["href"] for a_tag in extras_tag.find_all('a')]
+
+        title_content = get_title_contents(title_tag)
+        if by_tag is None:
+            by_content = (None,)
+        else:
+            by_content = get_by_contents(by_tag)
+        extras_content = get_extras_content(extras_tag)
 
         product_info["name"] = title_content[0]
-        product_info["description"] = title_content[-1]
-        product_info["link"] = title_tag.a["href"]
-        product_info["is_solo_maker"] = check_filter(extras_links, "soloMaker")
-        product_info["is_bootstrapped"] = check_filter(extras_links, "bootstrapped")
-        product_info["topics"] = get_topics(extras_links)
+        product_info["description"] = title_content[1]
+        product_info["link"] = title_content[2]
+        product_info["by"] = by_content[0]
+        product_info["is_solo_maker"] = extras_content[0]
+        product_info["is_bootstrapped"] = extras_content[1]
+        product_info["topics"] = extras_content[2]
 
         products.append(product_info)
     return products
 
+def get_title_contents(tag:Tag):
+    a_tag = tag.find('a')
+    content = list(a_tag.stripped_strings)
+    return (content[0],
+            content[-1],
+            a_tag['href'])
+
+def get_by_contents(tag:Tag):
+    a_tag = tag.find('a')
+    return (a_tag['href'],)
+
+
+def get_extras_content(tag:Tag):
+    links = [a_tag["href"] for a_tag in tag.find_all('a')]
+    solo_maker_filter   = check_filter(links, "soloMaker")
+    bootstrapped_filter = check_filter(links, "bootstrapped")
+    topics = get_topics(links)
+
+    return (solo_maker_filter,
+            bootstrapped_filter,
+            topics)
 
 def check_filter(extras:list[str], filter:str) -> bool:
     """
@@ -66,7 +95,7 @@ def check_filter(extras:list[str], filter:str) -> bool:
 
 def get_topics(extras:list[str]):
     """Extract topics from extras"""
-    topics = [link.split("/")[-1] for link in extras if link.startswith("/topics")]
+    topics = [link for link in extras if link.startswith("/topics")]
     return topics
 
 
