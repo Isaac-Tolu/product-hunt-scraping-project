bacf3684bac4445d0b3ed32b4c41ab24cc76cb2e
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-02-01 10:37:41 +0100

    fix: had to change the class attribute for the by section; would have to work on a better approach as class names change

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ PARSER = 'html.parser'
 
 PRODUCT_SECTION__CLASS    = "styles_item__Dk_nz"
 PRODUCT_TITLE__CLASS      = "styles_titleContainer__qZRNa"
-PRODUCT_BY__CLASS         = "styles_container__EoS8Q"
+PRODUCT_BY__CLASS         = "styles_container__br9Sy"
 PRODUCT_EXTRAS__CLASS     = "styles_extraInfo__Xs_5Y"
 PRODUCT_VOTE_BUTTON__CLASS = "styles_voteCountItem__zwuqk"
 
