0aa167db204e98a20d353579565e9400ce3de1fe
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-02-01 15:58:39 +0100

    added constants to a separate file

---
+++ phunt/constants.py
@@ -0,0 +1,6 @@
+
+PRODUCT_SECTION__CLASS    = "styles_item__Dk_nz"
+PRODUCT_TITLE__CLASS      = "styles_titleContainer__qZRNa"
+PRODUCT_BY__CLASS         = "styles_container__br9Sy"
+PRODUCT_EXTRAS__CLASS     = "styles_extraInfo__Xs_5Y"
+PRODUCT_VOTE_BUTTON__CLASS = "styles_voteCountItem__zwuqk"
