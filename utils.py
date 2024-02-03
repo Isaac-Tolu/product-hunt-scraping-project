1e16b418f4a023b3dc514234aa56247f54f6e771
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-02-03 10:32:49 +0100

    now using csv library to create csv file

--- utils.py
+++ utils.py
@@ -1,18 +1,14 @@
+import csv
 
 def to_csv(data:list[dict]):
     """Given a list of dictionaries, create a CSV file from the data"""
 
-    keys = data[0].keys()
-    header = ','.join(keys)
+    with open('result.csv', 'w', newline='') as csvfile:
+        fieldnames = data[0].keys()
 
-    with open("result.csv", 'w') as f:
-        f.write(header+'\n')
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    for i in data:
-        #row = ','.join(i.values())
-        row = ""
-        for val in i.values():
-            row = row + str(val) + ','
-        with open("result.csv", 'a') as f:
-            f.write(row.rstrip(',')+'\n')
+        writer.writeheader()
+        for i in data:
+            writer.writerow(i)
 
